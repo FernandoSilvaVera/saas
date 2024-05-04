a0acc18f31a5d701d45c78775dee4c16a6438e73
fernandosilvavera <fernandosilv4c@gmail.com>
2024-05-04 19:34:16 +0000

    Auth incorporado

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\PlansController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\DownloadController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,13 +13,52 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
+require __DIR__.'/auth.php';
+
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+	return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::get('/', [AppController::class, 'index']);
+Route::get('/app', [AppController::class, 'index']);
+
+Route::get('/plans', [PlansController::class, 'index']);
+Route::get('/buy/{id}', [PlansController::class, 'buy']);
+
+Route::get('/createUsers', [UsersController::class, 'createUsers']);
+Route::get('/listUsers', [UsersController::class, 'listUsers']);
+
+Route::post('/download', [DownloadController::class, 'download'])->name('download');
+
+Route::post('/preview', [DownloadController::class, 'preview'])->name('preview-document');
+
+Route::get('/history', [HistoryController::class, 'listHistory']);
+
+Route::get('/newPlan', function () {
+	return view('newPlan');
+});
+
+Route::get('/contact', function () {
+	return view('contact');
+});
+
+Route::get('/template', function () {
+	return view('template');
+});
+
+
+
+Route::get('/account', function () {
+	return view('account');
+});
+
+Route::get('/customize', function () {
+	return view('customize');
+});
+
+
